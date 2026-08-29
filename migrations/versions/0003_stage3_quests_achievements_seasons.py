b6573fd107b45a29216861f86f44fb05341d4fee
Dilorian <118097041+Dilorian@users.noreply.github.com>
2026-08-29 17:16:06 +0600

    Update 0003_stage3_quests_achievements_seasons.py

--- migrations/versions/0003_stage3_quests_achievements_seasons.py
+++ migrations/versions/0003_stage3_quests_achievements_seasons.py
@@ -11,7 +11,7 @@ from typing import Sequence, Union
 import sqlalchemy as sa
 from alembic import op
 
-revision: str = "0003_stage3_quests_achievements_seasons"
+revision: str = "0003_stage3"
 down_revision: Union[str, None] = "0002_stage2"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
