1bbbae28e87a493b89dc370903a058bc7516d05b
Dilorian <118097041+Dilorian@users.noreply.github.com>
2026-08-29 21:33:27 +0600

    Update 0004_stage4_economy.py

--- migrations/versions/0004_stage4_economy.py
+++ migrations/versions/0004_stage4_economy.py
@@ -9,7 +9,7 @@ from alembic import op
 import sqlalchemy as sa
 
 revision: str = "0004_stage4"
-down_revision: Union[str, None] = "0003_stage3"
+down_revision: Union[str, None] = "0003_stage3_quests_achievements_seasons"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
